a12cbfd5b508b40570a5fe7789eb604c3d7ecc90
Bruno Alvisio <bruno.alvisio@gmail.com>
2020-02-04 13:39:40 -0800

    Added minimal plugins

---
+++ plugin_1/plugin.lua
@@ -0,0 +1,6 @@
+-- Name: Plugin 1
+-- Version: 1.0
+-- Description: This is version 1.0 of plugin 1
+function run(input)
+    return "plugin 1 version 1.0"
+end
---
+++ plugin_2/plugin.lua
@@ -0,0 +1,6 @@
+-- Name: Plugin 2
+-- Version: 1.0
+-- Description: This is plugin 2
+function run(input)
+    return "plugin 2 version 1.0"
+end
