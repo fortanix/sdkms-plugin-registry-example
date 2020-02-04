873127761007bf5255c9972cf96bb944a1c4c38a
Bruno Alvisio <bruno.alvisio@gmail.com>
2020-02-04 13:40:14 -0800

    Added version 2.0 of plugin 1

--- plugin_1/plugin.lua
+++ plugin_1/plugin.lua
@@ -1,6 +1,6 @@
 -- Name: Plugin 1
--- Version: 1.0
--- Description: This is version 1.0 of plugin 1
+-- Version: 2.0
+-- Description: This is version 2.0 of plugin 1
 function run(input)
-    return "plugin 1 version 1.0"
+    return "plugin 1 version 2.0"
 end
